3d08242b34fa36a8a4af3b8e82ff752453807a4f
László Molnár <laszlo.molnar25@gmail.com>
2019-01-17 15:40:08 +0100

    Add BMI.java, DefineBasicInfo.java, VariableMutation.java

---
+++ week-01/day-4/src/DefineBasicInfo.java
@@ -0,0 +1,21 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        // Your age as an integer
+        // Your height in meters as a double
+        // Whether you are married or not as a boolean
+
+        String name = "László Molnár";
+        int age = 30;
+        double height = 1.82;
+        boolean married = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(married);
+
+
+    }
+}
