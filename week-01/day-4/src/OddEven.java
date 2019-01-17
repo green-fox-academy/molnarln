dd62ec8425a7a73c4fdf9d3c84ee352ee957b974
László Molnár <laszlo.molnar25@gmail.com>
2019-01-17 16:48:14 +0100

    Add AverageOfInput.java

---
+++ week-01/day-4/src/OddEven.java
@@ -0,0 +1,9 @@
+public class OddEven {
+
+    public static void main(String[] args) {
+        // Write a program that reads a number from the standard input,
+        // Then prints "Odd" if the number is odd, or "Even" if it is even.
+        
+
+    }
+}
