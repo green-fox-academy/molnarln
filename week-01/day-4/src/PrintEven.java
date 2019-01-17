fdd93a35ad2d7fa05aeef03ab5ed33b0e4a3efa5
László Molnár <laszlo.molnar25@gmail.com>
2019-01-17 20:47:39 +0100

    Add IWontCheatOnTheExam.java, MultiplicationTable.java, PrintEven.java

---
+++ week-01/day-4/src/PrintEven.java
@@ -0,0 +1,11 @@
+public class PrintEven {
+    public static void main(String[] args) {
+        // Create a program that prints all the even numbers between 0 and 500
+
+        for (int i = 0; i < 501; i++){
+            System.out.println(i);
+
+        }
+    }
+
+}
