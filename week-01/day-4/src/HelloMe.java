9b9770df1a889767f9a4ab4f4753dc17c6172706
László Molnár <laszlo.molnar25@gmail.com>
2019-01-17 12:43:27 +0100

    Add HelloMe.java and HumptyDumpty.java.

---
+++ week-01/day-4/src/HelloMe.java
@@ -0,0 +1,10 @@
+class HelloMe {
+    public static void main(String[] args) {
+
+
+        // Modify this program to greet you instead of the World!
+        System.out.println("Hello, László Molnár");
+
+
+    }
+}
--- week-01/day-4/src/day4workshop.java
+++
@@ -1,7 +0,0 @@
-public class day4workshop {
-}
-public static void main () {
-
-
-
-}
