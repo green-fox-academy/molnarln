01992cc20ae19c373e6930498e84581c2f8ab647
László Molnár <laszlo.molnar25@gmail.com>
2019-01-17 12:55:13 +0100

    Add HelloOthers.java

---
+++ week-01/day-4/src/HelloOthers.java
@@ -0,0 +1,15 @@
+class HelloOthers {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+
+        System.out.println("Hello Bálint!");
+        System.out.println("Hello Dezső!");
+        System.out.println("Hello Szabi!");
+
+    }
+}
