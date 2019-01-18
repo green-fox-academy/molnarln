3f06a24fd5cc3f06306aef435f8a4fed68542415
László Molnár <laszlo.molnar25@gmail.com>
2019-01-18 22:01:02 +0100

    Fix DrawPyramid.java

--- week-01/day-4/src/DrawPyramid.java
+++ week-01/day-4/src/DrawPyramid.java
@@ -23,8 +23,8 @@ public class DrawPyramid {
                 System.out.print(" ");
 
             }
-            for (int k = 0; k < i; k++) {
-                System.out.print("* ");
+            for (int k = 0; k < (2*i)-1; k++) {
+                System.out.print("*");
             }
             System.out.println();
 
