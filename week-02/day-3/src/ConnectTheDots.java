bb60fe6750434cd5ee56be475b03c29880eb81ae
László Molnár <laszlo.molnar25@gmail.com>
2019-01-23 16:52:24 +0100

    Add ConnectTheDots.java

---
+++ week-02/day-3/src/ConnectTheDots.java
@@ -0,0 +1,74 @@
+import javax.swing.*;
+
+import java.awt.*;
+import java.util.Arrays;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import static javax.swing.JFrame.isDefaultLookAndFeelDecorated;
+
+public class ConnectTheDots {
+    public static void mainDraw(Graphics graphics) {
+        // Create a function that takes 2 parameters:
+        // An array of {x, y} points and graphics
+        // and connects them with green lines.
+        // Connect these to get a box: {{10, 10}, {290,  10}, {290, 290}, {10, 290}}
+        // Connect these: {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
+        // {120, 100}, {85, 130}, {50, 100}}
+
+        int x = 30;
+        int y = 30;
+        connect(x, y, graphics);
+    }
+    public static void connect (int x, int y, Graphics graphics){
+        int a [][] = {{10, 10}, {290, 10}, {290, 290}, {10, 290}};
+        int b [][] = {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},{120, 100}, {85, 130}, {50, 100}};
+
+/*        for (int i = 0; i < a.length-1; i++) {
+            for (int j = 0; j <a[i].length-1; j++) {
+                graphics.setColor(Color.BLACK);
+                graphics.drawLine(a[i][j], a[i][j+1], a[i+1][j], a[i+1][j+1]);
+            }
+        }*/
+
+        int [] ax = new int[a.length];
+        int [] ay = new int[a.length];
+        int [] bx = new int[b.length];
+        int [] by = new int[b.length];
+
+        for (int i = 0; i < a.length; i++) {
+            ax[i] = a[i][0];
+            ay[i] = a[i][1];
+        }
+        graphics.drawPolygon(ax, ay, ax.length);
+
+
+        for (int i = 0; i < b.length; i++) {
+            bx[i] = b[i][0];
+            by[i] = b[i][1];
+        }
+        graphics.drawPolygon(bx, by, bx.length);
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
