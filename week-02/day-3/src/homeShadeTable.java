d1530160e94a3b305b22174b5dbe7b3823aafa2f
László Molnár <laszlo.molnar25@gmail.com>
2019-01-23 21:34:51 +0100

    homeShadeTable.java

---
+++ week-02/day-3/src/homeShadeTable.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class homeShadeTable {
+    public static void mainDraw(Graphics graphics) {
+        // Fill the canvas with a checkerboard pattern.
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9 ; j++) {
+                graphics.setColor(new Color(j*30));
+                graphics.fillRect(j*20*2, i*20*2, 30, 30);
+            }
+        }
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
