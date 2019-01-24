003af9771d5c172c32c7e46b0bd0ae987e7953ff
László Molnár <laszlo.molnar25@gmail.com>
2019-01-24 14:45:29 +0100

    Add MultipleLinePlay.java

---
+++ week-02/day-4/src/MultipleLinePlay.java
@@ -0,0 +1,56 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class MultipleLinePlay {
+    public static void mainDraw(Graphics graphics) {
+        int multiplyer = 2;
+        int [] x = {20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 320};
+        int [] y = {20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 320};
+
+        for (int i = 0; i < multiplyer ; i++) {
+            for (int k = 0; k < multiplyer; k++) {
+
+            graphics.setColor(Color.MAGENTA);
+            for (int j = 0; j < x.length; j++) {
+                graphics.drawLine((x[j]/multiplyer)+i*320/multiplyer, 0+k*320/multiplyer, (320/multiplyer)+i*320/multiplyer, x[j]/multiplyer+k*320/multiplyer);
+
+            }
+            graphics.setColor(Color.cyan);
+            for (int j = 0; j < y.length; j++) {
+                graphics.drawLine(y[j]/multiplyer+i*320/multiplyer, 320/multiplyer+k*320/multiplyer, 0+i*320/multiplyer, y[j]/multiplyer+k*320/multiplyer);
+
+            }
+            }
+
+
+        }
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
