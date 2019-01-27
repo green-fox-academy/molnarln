0a7a4374289db87bdac7a22f4938228b6c0e55a1
László Molnár <laszlo.molnar25@gmail.com>
2019-01-27 12:58:00 +0100

    Add SubStr.java

--- week-01/day-4/src/SubStr.java
+++ week-01/day-4/src/SubStr.java
@@ -1,41 +1,30 @@
-/*
 import java.util.Scanner;
 
 public class SubStr {
     public static void main(String[] args) {
-        public class SubStr {
-            public static void main(String[] args) {
-                //  Create a function that takes two strings as a parameter
-                //  Returns the starting index where the second one is starting in the first one
-                //  Returns `-1` if the second string is not in the first one
+        //  Create a function that takes two strings as a parameter
+        //  Returns the starting index where the second one is starting in the first one
+        //  Returns `-1` if the second string is not in the first one
 
-                //  Example:
-                System.out.println(subStr("this is what I'm searching in", "searching"));
-                //  should print: `17`
-                System.out.println(subStr("this is what I'm searching in", "not"));
-                //  should print: `-1`
-            }
-
-            public static int subStr(String input, String q) {
-
-                System.out.println("Give the first string!");
-                Scanner scanner = new Scanner(System.in);
-
-                String firstString = scanner.toString();
-
-                System.out.println("Give the second string!");
-                String secondString = scanner.toString();
-
-                for (int i = 0; i < firstString.length(); i++){
-                    if(char i  = ){
-
-                    }
-                }
-
-                return -1;
-            }
-        }
+        //  Example:
+        System.out.println(subStr("this is what I'm searching in", "searching"));
+        //  should print: `17`
+        System.out.println(subStr("this is what I'm searching in", "not"));
+        //  should print: `-1`
     }
 
+    public static int subStr(String input, String q) {
+/*        Scanner scanner = new Scanner(System.in);
+        input = scanner.nextLine();
+        q = scanner.next();*/
+
+        for (int i = 0; i < input.length()-q.length(); i++) {
+
+            if(input.substring(i, i+q.length()).equals(q)){
+
+                return i;
+            }
+        }
+        return -1;
+    }
 }
-*/
