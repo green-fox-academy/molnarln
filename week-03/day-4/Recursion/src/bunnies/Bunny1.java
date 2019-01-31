56247b3e9e2211dc6299ae17b4a1ce0a31321222
László Molnár <laszlo.molnar25@gmail.com>
2019-01-31 11:55:04 +0100

    Add Power.java, Bunny1,java

---
+++ week-03/day-4/Recursion/src/bunnies/Bunny1.java
@@ -0,0 +1,9 @@
+package bunnies;
+
+public class Bunny1 {
+    public static void main(String[] args) {
+        // We have a number of bunnies and each bunny has two big floppy ears.
+        // We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+    }
+    
+}
---
+++ week-03/day-4/Recursion/src/power/Power.java
@@ -0,0 +1,20 @@
+package power;
+
+public class Power {
+    // Given base and n that are both 1 or more, compute recursively (no loops)
+    // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+    public static void main(String[] args) {
+        int base = 3;
+        int power = 2;
+        System.out.println(toPower(base, power));
+    }
+
+    public static int toPower(int base, int power){
+
+        if(power > 1){
+            return base * toPower(base,power-1);
+        }
+
+        return base;
+    }
+}
