4692da2a200c1d4f6927a17be807f7dee91e6a8f
László Molnár <laszlo.molnar25@gmail.com>
2019-02-03 11:53:18 +0100

    Add PiratesMain.java

--- week-03/day-3/ClassesAndFields/src/pirates/Pirate.java
+++ week-03/day-3/ClassesAndFields/src/pirates/Pirate.java
@@ -2,18 +2,28 @@ package pirates;
 
 public class Pirate {
     int levelOfIntoxication;
-
+    boolean isDead;
     public Pirate(){
         levelOfIntoxication = 0;
     }
     public void drinkSomeRum(){
-        levelOfIntoxication ++;
+        if (!isDead){
+            levelOfIntoxication ++;
+        } else {
+            System.out.println("He is dead.");
+        }
     }
     public void howItGoingMate(){
-        if (0 <= levelOfIntoxication && levelOfIntoxication <= 4){
+        if (0 <= levelOfIntoxication && levelOfIntoxication <= 4 && !isDead){
             System.out.println("Pour me anudder!");
-        }else {
+        }else if(levelOfIntoxication > 4 && !isDead) {
             System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
+        }else if(isDead){
+            System.out.println("He is dead.");
         }
+    }
+
+    public void die(){
+        isDead = true;
     }
 }
---
+++ week-03/day-3/ClassesAndFields/src/pirates/PiratesMain.java
@@ -0,0 +1,24 @@
+package pirates;
+
+public class PiratesMain {
+    public static void main(String[] args) {
+        Pirate jackSparrow = new Pirate();
+
+        jackSparrow.drinkSomeRum();
+        jackSparrow.drinkSomeRum();
+        jackSparrow.drinkSomeRum();
+        jackSparrow.drinkSomeRum();
+        jackSparrow.drinkSomeRum();
+
+        jackSparrow.howItGoingMate();
+
+        jackSparrow.die();
+        jackSparrow.drinkSomeRum();
+        jackSparrow.howItGoingMate();
+
+
+
+
+
+    }
+}
