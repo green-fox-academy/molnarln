f3eb6f7c8ae4aed6d8ee80521179c6dd1d80a65a
László Molnár <laszlo.molnar25@gmail.com>
2019-02-07 19:36:33 +0100

    Solves in another way MostCommonChar.java, MostCommonCharMain.java

---
+++ week-04/day-4/testing/src/MostCommonChar.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MostCommonChar {
+
+    public char countLetters (String inputString) {
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        ArrayList<Character> arrayFromString = new ArrayList<>();
+
+        for (char c: inputString.toCharArray()
+        ) {
+            arrayFromString.add(c);
+        }
+        char currentUniqueChar = ' ';
+        for (char c : arrayFromString) {
+            int currentNumOccurence = 1;
+
+            for (char z : arrayFromString) {
+                if (z == c){
+                    map.put(c, currentNumOccurence);
+                    currentNumOccurence++;
+                }
+            }
+        }
+        int maxvalue = 0;
+        char mostCommon = ' ';
+        for (int value: map.values()
+             ) {
+            if (value > maxvalue){
+                maxvalue = value;
+            }
+        }
+        for (char c: map.keySet()
+             ) {
+            if (map.get(c).equals(maxvalue)){
+                mostCommon = c;
+            }
+        }
+        return mostCommon;
+    }
+}
---
+++ week-04/day-4/testing/src/MostCommonCharMain.java
@@ -0,0 +1,11 @@
+public class MostCommonCharMain {
+
+    public static void main(String[] args) {
+
+        String text= "counootc";
+
+        MostCommonChar testFunction = new MostCommonChar();
+        System.out.println(testFunction.countLetters(text));
+
+    }
+}
