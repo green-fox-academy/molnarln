849024fe08bf2a6833387c0055f64af783c2ced7
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-03-12 11:33:19 +0100

    Minor change in TodoController.java

--- week-08/day-1/connection_with_mysql/src/main/java/com/greenfoxacademy/connection_with_mysql/controller/TodoController.java
+++ week-08/day-1/connection_with_mysql/src/main/java/com/greenfoxacademy/connection_with_mysql/controller/TodoController.java
@@ -33,7 +33,7 @@ public class TodoController {
                     .filter(e -> !e.isDone())
                     .collect(Collectors.toList());
             model.addAttribute("todos", onlyActive);
-        }else {
+        } else {
 
             model.addAttribute("todos", todoRepository.findAll());
         }
@@ -41,31 +41,31 @@ public class TodoController {
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
-    public String showAddForm(Model model){
+    public String showAddForm(Model model) {
         model.addAttribute("todo", new Todo());
         return "addtodo";
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String addNewTodo(@ModelAttribute Todo todo){
+    public String addNewTodo(@ModelAttribute Todo todo) {
         todoRepository.save(todo);
         return "redirect:/todo/";
     }
 
     @RequestMapping(value = "/{id}/delete", method = RequestMethod.GET)
-    public String deleteTodo(@PathVariable("id") Long id){
+    public String deleteTodo(@PathVariable("id") Long id) {
         todoRepository.deleteById(id);
         return "redirect:/todo/";
     }
 
     @RequestMapping(value = "/{id}/update", method = RequestMethod.GET)
-    public String showUpdateView(@PathVariable("id") Long id, Model model){
+    public String showUpdateView(@PathVariable("id") Long id, Model model) {
         model.addAttribute("todo", todoRepository.findById(id).orElseThrow(NullPointerException::new));
         return "update";
     }
 
     @RequestMapping(value = "/{id}/update", method = RequestMethod.POST)
-    public String updateTodo(@PathVariable Long id,Todo todo){
+    public String updateTodo(@PathVariable Long id, Todo todo) {
         Todo todoToUpdate = todoRepository.findById(id).get();
         todoToUpdate.setTitle(todo.getTitle());
         todoToUpdate.setDone(todo.isDone());
