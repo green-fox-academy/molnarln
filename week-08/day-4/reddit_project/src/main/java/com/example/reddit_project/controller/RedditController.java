50615076b539c1d659005d20157e73aa16d06618
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-03-14 17:37:38 +0100

    Minor changes in reddit project

--- week-08/day-4/reddit_project/src/main/java/com/example/reddit_project/controller/RedditController.java
+++ week-08/day-4/reddit_project/src/main/java/com/example/reddit_project/controller/RedditController.java
@@ -2,7 +2,6 @@ package com.example.reddit_project.controller;
 
 import com.example.reddit_project.model.Post;
 import com.example.reddit_project.service.PostService;
-import com.sun.org.apache.regexp.internal.RE;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,10 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.List;
-
 @Controller
-
 public class RedditController {
 
     PostService postService;
--- week-08/day-4/reddit_project/src/main/java/com/example/reddit_project/service/PostService.java
+++ week-08/day-4/reddit_project/src/main/java/com/example/reddit_project/service/PostService.java
@@ -6,11 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
-
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+
 public class PostService {
 
 
@@ -44,11 +44,11 @@ public class PostService {
         return this.postRepository.findAllByOrderByVotesDesc(pageable);
     }
 
-    public List<Integer> pages(){
+    public List<Integer> pages() {
         List<Integer> allPages = new ArrayList<>();
         long numberOfPosts = postRepository.countAllBy();
-        long maxPage = numberOfPosts/10;
-        for (int i = 0; i <= maxPage ; i++) {
+        long maxPage = numberOfPosts / 10;
+        for (int i = 0; i <= maxPage; i++) {
             allPages.add(i);
         }
         return allPages;
