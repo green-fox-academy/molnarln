405886c5bf2bef9bdb31af95106528c1973c62ce
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-03-14 10:24:31 +0100

    Initialize reddit project

---
+++ week-08/day-4/reddit_project/src/main/java/com/example/reddit_project/RedditProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.reddit_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedditProjectApplication.class, args);
+    }
+
+}
