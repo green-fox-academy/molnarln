c5744b4c6d5c2ace6a4485ed8eeaf1ee1b5b4072
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-03-15 10:03:57 +0100

    Add custom error controller to reddit project

---
+++ week-08/day-4/reddit_project/src/main/java/com/example/reddit_project/controller/CustomErrorController.java
@@ -0,0 +1,20 @@
+package com.example.reddit_project.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        //do something like logging
+        return "error";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
