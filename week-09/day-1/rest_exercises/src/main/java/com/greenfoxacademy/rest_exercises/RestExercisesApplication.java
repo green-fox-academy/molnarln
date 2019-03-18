0bbfbb5cb13fac273eaeaf7afc3b8fa13f241cfe
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-03-18 13:34:23 +0100

    Initialize rest exercise spring setup

---
+++ week-09/day-1/rest_exercises/src/main/java/com/greenfoxacademy/rest_exercises/RestExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.rest_exercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestExercisesApplication.class, args);
+    }
+
+}
