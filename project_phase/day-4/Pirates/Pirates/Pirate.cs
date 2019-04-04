7b68edaddc92c7cd26d6898040ec44fa65586c1f
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-04-04 14:34:53 +0200

    Add fields and methods for pirate class

---
+++ project_phase/day-4/Pirates/Pirates/Pirate.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pirates
+{
+    class Pirate
+    {
+        private string name;
+        public string Name { get => name; set => name = value; }
+
+        private int levelOfIntoxication;
+        public int LevelOfIntoxication { get => levelOfIntoxication; set => levelOfIntoxication = value; }
+
+        private bool isSleeping;
+        public bool IsSleeping { get => isSleeping; set => isSleeping = value; }
+
+        private bool isDead;
+        public bool IsDead { get => isDead; set => isDead = value; }
+
+
+        public Pirate()
+        {
+            this.isSleeping = false;
+            this.isDead = false;
+            this.LevelOfIntoxication = 0;
+        }
+
+        public Pirate(string name)
+        {
+            this.isSleeping = false;
+            this.isDead = false;
+            this.LevelOfIntoxication = 0;
+            this.name = name;
+        }
+
+        public void DrinkSomeRum()
+        {
+            if (isDead)
+            {
+                Console.WriteLine("he is dead");
+            }
+            else
+            {
+                this.LevelOfIntoxication++;
+            }
+        }
+
+        public void HowItIsGoingMate()
+        {
+            if (isDead)
+            {
+                Console.WriteLine("he is dead");
+            }
+            else
+            {
+                if (this.levelOfIntoxication <= 4)
+                {
+                    Console.WriteLine("Pour me anudder!");
+                }
+                else
+                {
+                    Console.WriteLine($"Arghh, I'm Pirate. How d'ya d'ink its goin?");
+                }
+            }
+        }
+
+        public void Die()
+        {
+            this.isDead = true;
+        }
+
+        public void Brawl(Pirate pirate)
+        {
+            if (this.isDead)
+            {
+                Console.WriteLine("he is dead");
+            }
+            else
+            {
+                if (!pirate.isDead)
+                {
+                    Random rnd = new Random();
+                    int random = rnd.Next(1, 3);
+                    if (random == 1)
+                    {
+                        this.isDead = true;
+                    }
+                    if (random == 2)
+                    {
+                        pirate.isDead = true;
+                    }
+                    if (random == 3)
+                    {
+                        this.isSleeping = true;
+                        pirate.isSleeping = true;
+                    }
+                }
+            }
+        }
+    }
+}
--- project_phase/day-4/Pirates/Pirates/Program.cs
+++ project_phase/day-4/Pirates/Pirates/Program.cs
@@ -10,12 +10,16 @@ namespace Pirates
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+
+            Pirate pirate1 = new Pirate();
+            Pirate pirate2 = new Pirate();
+
+            pirate1.Brawl(pirate2);
+
+            Console.WriteLine(pirate2.IsDead);
+
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
 }
