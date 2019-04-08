985d47e1c05e27567dd2fd13cbc5e05554a5900c
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-04-08 16:55:08 +0200

    Add formatting

--- project_phase/week-01/day-1/BankOfSymba/BankOfSymba/Models/AccountsShowViewModel.cs
+++ project_phase/week-01/day-1/BankOfSymba/BankOfSymba/Models/AccountsShowViewModel.cs
@@ -11,9 +11,8 @@ namespace BankOfSymba.Models
 
         public AccountsShowViewModel()
         {
-            this.BankAccounts = new List<BankAccount>
-                ()
-            {   new BankAccount("Symba", 12000, "Lion"),
+            this.BankAccounts = new List<BankAccount>()
+            {   new BankAccount("Symba", 12000, "Lion"){IsKing = true},
                 new BankAccount("Pumba", 2000, "pig"),
                 new BankAccount("Nala", 34000, "Lion")
             };
--- project_phase/week-01/day-1/BankOfSymba/BankOfSymba/Models/BankAccount.cs
+++ project_phase/week-01/day-1/BankOfSymba/BankOfSymba/Models/BankAccount.cs
@@ -7,17 +7,22 @@ namespace BankOfSymba.Models
 {
     public class BankAccount
     {
+        private static int GlobalId { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public int BalanceInt { get; set; }
         public string AnimalType { get; set; }
         public string BalanceString { get; set; }
+        public bool IsKing { get; set; }
 
         public BankAccount(string name, int balance, string animalType)
         {
+            GlobalId++;
             Name = name;
             BalanceInt = balance;
             AnimalType = animalType;
             this.BalanceString = BalanceInt.ToString("F");
+            this.Id = GlobalId;
         }
     }
 }
