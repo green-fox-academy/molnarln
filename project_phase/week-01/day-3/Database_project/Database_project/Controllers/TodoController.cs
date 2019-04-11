7d37dc1ce0e6f9b045780fda36b738d5a08707ea
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-04-11 20:46:03 +0200

    Add Fluent API, works well

--- project_phase/week-01/day-3/Database_project/Database_project/Controllers/TodoController.cs
+++ project_phase/week-01/day-3/Database_project/Database_project/Controllers/TodoController.cs
@@ -1,11 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Database_project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Database_project.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace Database_project.Controllers
 {
