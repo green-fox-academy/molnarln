b933f2ed493b4a178ebee32375cbce81da7042b7
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-04-12 11:34:06 +0200

    Minor changes

--- project_phase/week-01/day-3/Database_project/Database_project/Startup.cs
+++ project_phase/week-01/day-3/Database_project/Database_project/Startup.cs
@@ -20,12 +20,10 @@ namespace Database_project
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            
             services.AddDbContext<ApplicationContext>(builder =>
                     builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
