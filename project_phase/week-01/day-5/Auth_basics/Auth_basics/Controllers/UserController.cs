8530e260e13efb4747e425b53b0147b769751f13
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-04-22 11:43:19 +0200

    Merge commit

---
+++ project_phase/week-01/day-5/Auth_basics/Auth_basics/Controllers/UserController.cs
@@ -0,0 +1,86 @@
+﻿using Auth_basics.Entities;
+using Auth_basics.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Auth_basics.Helpers;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Extensions.Options;
+using System.Text;
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Auth_basics.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+        private AppSettings _appSettings;
+
+        public UsersController(IUserService userService, IOptions<AppSettings> appSettings)
+        {
+            _userService = userService;
+            _appSettings = appSettings.Value;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody]User userParam)
+        {
+            var user = _userService.Authenticate(userParam.Username, userParam.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            return Ok(users);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("checktoken")]
+        public IActionResult CheckTokenIsValid([FromHeader]string authorization)
+        {
+            var token = authorization.Replace("Bearer ", "");
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+
+            var validationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken validatedToken = null;
+            try
+            {
+                tokenHandler.ValidateToken(token, validationParameters, out validatedToken);
+            }
+            catch (SecurityTokenException)
+            {
+                return StatusCode(401, new { TokenIsVlaid = "false" });
+            }
+            catch (Exception e)
+            {
+
+                return StatusCode(111, new { message = e.ToString() });
+            }
+
+            //... manual validations return false if anything untoward is discovered
+            return Ok(validatedToken);
+        }
+    }
+}
---
+++ project_phase/week-01/day-5/Auth_basics/Auth_basics/Services/UserService.cs
@@ -0,0 +1,78 @@
+﻿using Auth_basics.Entities;
+using Auth_basics.Helpers;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
+
+namespace Auth_basics.Services
+{
+    public interface IUserService
+    {
+        User Authenticate(string username, string password);
+        IEnumerable<User> GetAll();
+    }
+
+    public class UserService : IUserService
+    {
+        // users hardcoded for simplicity, store in a db with hashed passwords in production applications
+        private List<User> _users = new List<User>
+        {
+            new User { Id = 1, FirstName = "Test", LastName = "User", Username = "test", Password = "test" },
+            new User { Id = 2, FirstName = "Test2", LastName = "User2", Username = "test2", Password = "test2" }
+        };
+
+        private readonly AppSettings _appSettings;
+
+        public UserService(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+
+        public User Authenticate(string username, string password)
+        {
+            var user = _users.SingleOrDefault(x => x.Username == username && x.Password == password);
+
+            // return null if user not found
+            if (user == null)
+                return null;
+
+            // authentication successful so generate jwt token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                }),
+                Expires = DateTime.UtcNow.AddDays(1),
+
+
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+
+            // remove password before returning
+            user.Password = null;
+
+            return user;
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            // return users without passwords
+            return _users.Select(x =>
+            {
+                x.Password = null;
+                return x;
+            });
+        }
+    }
+}
