63e73dd61de047ef82c14a395972a2f90d7274e3
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-04-23 21:41:59 +0200

    Fix development and production environment variables

--- project_phase/week-03/day-1/Environment_Practice/Database_project/Database_project/Startup.cs
+++ project_phase/week-03/day-1/Environment_Practice/Database_project/Database_project/Startup.cs
@@ -22,19 +22,16 @@ namespace Database_project
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //if (env.IsDevelopment())
-            //{
+            if (env.IsDevelopment())
+            {
                 services.AddDbContext<ApplicationContext>(builder =>
-                        builder.UseSqlServer(Configuration["TestString:DefaultConnection"]));
-
-             //   System.Console.WriteLine("ISDEVELOPEMENT");
-            //}
-            //if (env.IsProduction())
-            //{
-            //    System.Console.WriteLine("ISPRODUCTION");
-            //    services.AddDbContext<ApplicationContext>(builder =>
-            //            builder.UseSqlServer(Configuration.GetConnectionString("ProductionConnection")));
-            //}
+                        builder.UseSqlServer(Configuration.GetConnectionString("DevelopmentConnection")));
+            }
+            if (env.IsProduction())
+            {
+                services.AddDbContext<ApplicationContext>(builder =>
+                        builder.UseSqlServer(Configuration.GetConnectionString("ProductionConnection")));
+            }
             services.AddMvc();
 
         }
